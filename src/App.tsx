0251e267eab0f34441e16f5c7ae0ba4f45ecc681
Roberto dos Santos Filho <robertodossantosfilho1001@gmail.com>
2020-08-25 12:21:28 -0400

    Colocando layout e etilos global na aplicaçao

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+
+import Layout from './components/Layout'
 import GlobalStyle from './styles/styleGlobal'
 
 
 function App() {
   return (
     <>
-      <h1>Discord Clone</h1>
+      <Layout/>
       <GlobalStyle/>
     </>
   );
