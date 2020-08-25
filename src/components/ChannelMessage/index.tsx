93153c2a3a9aaaaf294ddd82286ec65c5e6312de
Roberto dos Santos Filho <robertodossantosfilho1001@gmail.com>
2020-08-25 15:36:39 -0400

    Mensagens dos usuarios

---
+++ src/components/ChannelMessage/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import {Container,Avatar,Message,Header,Content} from './styles'
+export {Mention} from './styles'
+
+export interface IMenssage{
+    author: string;
+    date: string;
+    content: string | React.ReactElement | React.ReactNode;
+    hasMentions?: boolean;
+    isBot?: boolean;
+}
+
+const ChannelMessage: React.FC<IMenssage> = ({
+    author,
+    date,
+    content,
+    hasMentions,
+    isBot
+}) => {
+    return(
+        <Container className={hasMentions ? 'mention' : ''}>
+            <Avatar className={isBot ? 'bot' : ''} />
+            <Message>
+                <Header>
+                    <strong>{author}</strong>
+                    {isBot && <span>Bot</span>}
+                    <time>{date}</time>
+                </Header>
+                <Content>{content}</Content>
+            </Message>
+        </Container>
+    )
+}
+
+export default ChannelMessage
